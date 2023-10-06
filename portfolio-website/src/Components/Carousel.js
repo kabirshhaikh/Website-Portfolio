added6a45b5612d64eb609842328c9b456930e34
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-10-06 10:39:28 -0400

    Implemented Caraousel, need to make it resposive

--- portfolio-website/src/Components/Carousel.js
+++ portfolio-website/src/Components/Carousel.js
@@ -10,33 +10,60 @@ import mumbai from "../Image/mumbai.jpeg";
 
 const Carousel = (props) => {
   const dataFromProps = props.data;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   const backButton = "<";
   const forwardButton = ">";
+
+  const handleBackward = () => {
+    if (currentIndex === 0) {
+      setCurrentIndex(dataFromProps.length - 1);
+    } else {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  const handleForward = () => {
+    if (currentIndex === dataFromProps.length - 1) {
+      setCurrentIndex(0);
+    } else {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
   return (
     <div className="carousel-container">
       <div className="back-button-container">
-        <Button variant="contained">{backButton}</Button>
+        <Button variant="contained" onClick={handleBackward}>
+          {backButton}
+        </Button>
       </div>
 
-      {dataFromProps.map((item) => {
+      {dataFromProps.map((item, index) => {
         return (
-          <div className="card-container">
+          <div
+            key={item.id}
+            className={`card-container ${
+              index === currentIndex ? "active" : ""
+            }`}
+          >
             <Card sx={{ maxWidth: 500 }}>
               <CardMedia
-                sx={{ height: 200, width: '100%' }}
-                image={mumbai}
+                sx={{ height: 200, width: "100%" }}
+                image={mumbai} // You may want to replace this with item-specific image
                 title="city image"
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
                   {item.titleName}
                 </Typography>
-                <Typography variant="subtitle1">{item.duration}, {item.place}</Typography>
+                <Typography variant="subtitle1">
+                  {item.duration}, {item.place}
+                </Typography>
                 <ul>
                   <li>Responsibility 1</li>
                   <li>Responsibility 2</li>
                   <li>Responsibility 3</li>
-                  {/* Add more responsibilities as needed */}
                 </ul>
               </CardContent>
               <CardActions>
@@ -49,7 +76,9 @@ const Carousel = (props) => {
       })}
 
       <div className="forward-button-container">
-        <Button variant="contained">{forwardButton}</Button>
+        <Button variant="contained" onClick={handleForward}>
+          {forwardButton}
+        </Button>
       </div>
     </div>
   );
